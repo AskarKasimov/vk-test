52e53daec02aa5fc12244351817addfe9bea739d
Askar Kasimov <kasimovaskar123@gmail.com>
2025-05-31 14:19:13 +0300

    feat: actual project structure

---
+++ src/features/CustomTable/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const CustomTable: FC = () => {
+  return <></>;
+};
+
+export default CustomTable;
--- src/main.tsx
+++ src/main.tsx
@@ -2,34 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import Home from './features/Home';
-import Additional from './features/Additional';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './shared/config/queryClient.ts';
-
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/second',
-        element: <Additional />,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <App />
     </QueryClientProvider>
     <ToastContainer
       position="bottom-right"
