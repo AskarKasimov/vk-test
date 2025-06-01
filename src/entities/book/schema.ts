069b7b79d33598e975f4efe431e034831d57dd4e
Askar Kasimov <kasimovaskar123@gmail.com>
2025-06-01 20:15:36 +0300

    fix: advanced scheme restrictions

--- src/entities/book/schema.ts
+++ src/entities/book/schema.ts
@@ -1,17 +1,45 @@
 import { z } from 'zod';
 
-const zParsedInt = z.preprocess((val: unknown) => {
-  const num = Number(val);
-  return isNaN(num) ? val : num;
-}, z.number());
+const zParsedInt = (
+  min: number,
+  minMessage?: string,
+  max?: number,
+  maxMessage?: string
+) =>
+  z.preprocess(
+    (val: unknown) => {
+      const num = Number(val);
+      return isNaN(num) ? val : num;
+    },
+    z
+      .number({ message: 'Должно быть числом' })
+      .int('Должно быть целым числом')
+      .min(min, minMessage || `Значение не может быть меньше ${min}`)
+      .max(
+        max ?? Number.MAX_SAFE_INTEGER,
+        maxMessage ||
+          `Значение не может быть больше ${max ?? Number.MAX_SAFE_INTEGER}`
+      )
+  );
 
 export const bookDTOSchema = z.object({
   id: z.string(),
-  title: z.string(),
-  author: z.string(),
-  genre: z.string(),
-  language: z.string(),
-  year: zParsedInt,
-  availableCopies: zParsedInt,
-  occupiedCopies: zParsedInt,
+  title: z.string().min(1, 'Название обязательно'),
+  author: z.string().min(1, 'Автор обязателен'),
+  genre: z.string().min(1, 'Жанр обязателен'),
+  language: z.string().min(1, 'Язык обязателен'),
+  year: zParsedInt(
+    0,
+    'Только наша эра',
+    new Date().getFullYear(),
+    'Год из будущего недопустим'
+  ),
+  availableCopies: zParsedInt(
+    0,
+    'Количество доступных копий не может быть меньше 0'
+  ),
+  occupiedCopies: zParsedInt(
+    0,
+    'Количество занятых копий не может быть меньше 0'
+  ),
 });
