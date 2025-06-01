8256d6cb6a044ac93857e3901f700b3bdf0a3bcc
Askar Kasimov <kasimovaskar123@gmail.com>
2025-06-01 20:57:51 +0300

    feat: more careful messages in scheme validation

--- src/entities/book/schema.ts
+++ src/entities/book/schema.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod';
 
 const zParsedInt = (
-  min: number,
+  fieldName: string,
+  min?: number,
   minMessage?: string,
   max?: number,
   maxMessage?: string
@@ -12,33 +13,40 @@ const zParsedInt = (
       return isNaN(num) ? val : num;
     },
     z
-      .number({ message: 'Должно быть числом' })
-      .int('Должно быть целым числом')
-      .min(min, minMessage || `Значение не может быть меньше ${min}`)
+      .number({ message: `Поле "${fieldName}" должно быть числом` })
+      .int(`Поле "${fieldName}" должно быть целым числом`)
+      .min(
+        min ?? Number.MIN_SAFE_INTEGER,
+        minMessage ||
+          `Значение поля "${fieldName}" не может быть меньше ${min ?? Number.MIN_SAFE_INTEGER}`
+      )
       .max(
         max ?? Number.MAX_SAFE_INTEGER,
         maxMessage ||
-          `Значение не может быть больше ${max ?? Number.MAX_SAFE_INTEGER}`
+          `Значение поля "${fieldName}" не может быть больше ${max ?? Number.MAX_SAFE_INTEGER}`
       )
   );
 
 export const bookDTOSchema = z.object({
   id: z.string(),
-  title: z.string().min(1, 'Название обязательно'),
-  author: z.string().min(1, 'Автор обязателен'),
-  genre: z.string().min(1, 'Жанр обязателен'),
-  language: z.string().min(1, 'Язык обязателен'),
+  title: z.string().min(1, 'Поле "Название" обязательно'),
+  author: z.string().min(1, 'Поле "Автор" обязательно'),
+  genre: z.string().min(1, 'Поле "Жанр" обязательно'),
+  language: z.string().min(1, 'Поле "Язык" обязательно'),
   year: zParsedInt(
+    'Год',
     0,
     'Только наша эра',
     new Date().getFullYear(),
     'Год из будущего недопустим'
   ),
   availableCopies: zParsedInt(
+    'Кол-во доступных копий',
     0,
     'Количество доступных копий не может быть меньше 0'
   ),
   occupiedCopies: zParsedInt(
+    'Кол-во занятых копий',
     0,
     'Количество занятых копий не может быть меньше 0'
   ),
